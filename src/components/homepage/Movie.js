73e1e9688e3d724614ff7e5ae1d79a1e23fa3d1a
Olaniran Sulaimon <suzzwayne@gmail.com>
2020-02-23 08:42:28 +0100

    changes to api call

--- src/components/homepage/Movie.js
+++ src/components/homepage/Movie.js
@@ -5,8 +5,8 @@ const Movie = (props) =>{
     return (
         <div className="movie-poster">
             {
-                props.image === null ? <img src ={`https://via.placeholder.com/150/000000/FFFFFF`} alt="Movie Poster" />
-                : <img src ={`https://image.tmdb.org/t/p/w185${props.image}`} alt="Movie Poster"/>
+                props.image === null ? <img src ={`http://via.placeholder.com/150/000000/FFFFFF`} alt="Movie Poster" />
+                : <img src ={`http://image.tmdb.org/t/p/w185${props.image}`} alt="Movie Poster"/>
 
                
             }
